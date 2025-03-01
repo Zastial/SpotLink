553401097ea63b2ab2abb8f1802da2c577ff5751
calulazone <lutinelo.lg@gmail.com>
2025-03-01 19:27:50 +0100

    change password ok

--- src/Controller/UserInformationsController.php
+++ src/Controller/UserInformationsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ChangePasswordFormType;
 use App\Form\UserInformationsFormType;
+use phpDocumentor\Reflection\Types\Null_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,13 +14,14 @@ use App\Entity\User;
 
 final class UserInformationsController extends AbstractController
 {
+    //Affichage de la vue user_informations
     #[Route('/user_informations', name: 'user_informations')]
     public function user_informations(Request $request, UserRepository $userRepository): Response
     {
         #TODO : Remplacer par user connected
         $user = $userRepository->findOneBy([], ['id' => 'ASC']);
         $userform = $this->createForm(UserInformationsFormType::class, $user);
-        $passwordform = $this->createForm(ChangePasswordFormType::class);
+
         $userform->handleRequest($request);
         if ($userform->isSubmitted() && $userform->isValid()) {
             $userRepository->save($user);
@@ -27,10 +29,10 @@ final class UserInformationsController extends AbstractController
 
         return $this->render('user_informations/userInformations.html.twig', [
             'userform' => $userform->createView(),
-            'passwordform' => $passwordform->createView(),
         ]);
     }
 
+    //Suppression du compte de l'utilisateur
     #[Route('/delete_account', name: 'delete_account')]
     public function delete_account(Request $request, UserRepository $userRepository): Response
     {
@@ -39,5 +41,26 @@ final class UserInformationsController extends AbstractController
         $userRepository->delete($user);
         return $this->redirectToRoute('app_home_page');
     }
-    
+
+    //Changement du mot de passe utilisateur
+    #[Route('/change_password', name: 'change_password', methods: ['POST'])]
+    public function change_password(Request $request, UserRepository $userRepository): Response
+    {
+        #TODO : Remplacer par user connected + hasher password
+        $data = $request->request->all();
+        $plainPassword = $data['password_field'];
+        $user = $userRepository->findOneBy([], ['id' => 'ASC']);
+        
+        if (!$this->isCsrfTokenValid('change_password', $request->request->get('_token'))) {
+            throw new \Exception('Invalid CSRF token');
+        }
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }else{
+            $user->setPassword($plainPassword);
+            $userRepository->save($user);
+            $this->addFlash('success', 'Votre mot de passe a été modifié avec succès.');   
+        }
+        return $this->redirectToRoute('user_informations');
+    }
 }
--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -18,39 +18,18 @@ class ChangePasswordFormType extends AbstractType
                 'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['placeholder' => 'Mot de passe'],
-                'constraints' => [
-                    new Assert\NotBlank(['message' => 'Veuillez entrer un mot de passe.']),
-                    new Assert\Length([
-                        'min' => 6,
-                        'minMessage' => 'Le mot de passe doit contenir au moins 6 caractères.',
-                    ]),
-                ],
             ])
             ->add('confirmPassword', PasswordType::class, [
                 'label' => 'Confirmer votre mot de passe',
                 'mapped' => false,
                 'attr' => ['placeholder' => 'Confirmez votre mot de passe'],
-                'constraints' => [
-                    new Assert\NotBlank(['message' => 'Veuillez confirmer votre mot de passe.']),
-                ],
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'validation_groups' => function ($form) {
-                return ['Default']; // Activation de la validation
-            },
+            'data_class' => null,
         ]);
-    }
-
-    public function validatePasswordMatch($data, ExecutionContextInterface $context): void
-    {
-        if ($data['password'] !== $data['confirmPassword']) {
-            $context->buildViolation("Les mots de passe ne correspondent pas.")
-                ->atPath('confirmPassword')
-                ->addViolation();
-        }
     }
 }
