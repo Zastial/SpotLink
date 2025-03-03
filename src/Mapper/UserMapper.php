12d688de803dfbaf892354647d25946934d27986
calulazone <lutinelo.lg@gmail.com>
2025-03-03 15:48:34 +0100

    fix mr

--- src/Mapper/UserMapper.php
+++ src/Mapper/UserMapper.php
@@ -11,10 +11,10 @@ class UserMapper
     public function mapDtoToEntity(UserDTO $dto, User $user): User
     {
         $user->setEmail($dto->email);
-        $user->setFirstName($dto->firstName);
-        $user->setLastName($dto->lastName);
+        $user->setFirstName($dto->first_name);
+        $user->setLastName($dto->last_name);
         $user->setUsername($dto->username);
-        $user->setIsVerify($dto->isVerify);
+        $user->setIsVerify($dto->is_verify);
 
         return $user;
     }
