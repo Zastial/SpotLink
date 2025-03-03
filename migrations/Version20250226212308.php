25c608913cafbd07465108379182a274054c1446
MathisMchd <michenaudmathis@gmail.com>
2025-03-03 09:14:46 +0100

    fix migration

--- migrations/Version20250226212308.php
+++ migrations/Version20250226212308.php
@@ -6,7 +6,6 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use App\Enum\Role;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -20,12 +19,14 @@ final class Version20250226212308 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql("INSERT INTO role (id, name) VALUES (1, 'USER')");
         $this->addSql("INSERT INTO role (id, name) VALUES (2, 'ADMIN')");
     }
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
     }
 }
