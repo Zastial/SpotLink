ff1887aaa8d196270395f89c086ce5ffbcd4f205
E21B719R <kenzo.buchy--petard@univ-nantes.fr>
2025-03-03 15:10:08 +0100

    fix responsive tableau + page admin accueil + gestion utilisateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Entity\User;
+use App\Entity\Role;
 use App\Enum\StatusEnum;
 use App\Repository\EventStatusRepository;
+use App\Repository\RoleRepository;
 use App\Repository\StatusRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class AdminController extends AbstractController
 {
@@ -27,7 +32,7 @@ final class AdminController extends AbstractController
     {
         // Au cas où on veut rajouter un filtre de recherche
         $search = $request->query->get('search', '');
-        $status = $request->query->get('status', -1);
+        $status = $request->query->get('status', 2);
 
         // Filtrer les événements
         $events = $eventRepository->getAllWithFilters($search, $status);
@@ -78,5 +83,54 @@ final class AdminController extends AbstractController
         $entityManagerInterface->flush();
 
         return $this->redirectToRoute('app_admin_events');
+    }
+
+    #[Route('/admin/users', name: 'app_admin_users')]
+    public function users(UserRepository $userRepositery, RoleRepository $roleRepository): Response
+    {
+        $users = $userRepositery->getAll();
+        $roles = $roleRepository->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'controller_name' => 'AdminController',
+            'users' => $users,
+            'roles' => $roles
+        ]);
+    }
+
+    #[Route('/admin/users/delete/{id}', name: 'app_admin_user_delete')]
+    public function userDelete(int $id,UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        if (!$user) {
+            #TODO : Remplacer par une notif utilisateur
+            throw new \Exception("Le compte est introuvable");
+        }
+        $userRepository->delete($user);
+
+        return $this->redirectToRoute('app_admin_users');
+    }
+
+    #[Route('/admin/update-role', name: 'admin_update_role', methods: ['POST'])]
+    public function updateRole(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['userId'], $data['newRoleId'])) {
+            return new JsonResponse(['success' => false, 'message' => 'Données invalides'], 400);
+        }
+
+        $user = $entityManager->getRepository(User::class)->find($data['userId']);
+        $role = $entityManager->getRepository(Role::class)->find($data['newRoleId']);
+
+        if (!$user || !$role) {
+            return new JsonResponse(['success' => false, 'message' => 'Utilisateur ou rôle introuvable'], 404);
+        }
+
+        $user->setRole($role);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true, 'message' => 'Rôle mis à jour']);
     }
 }
